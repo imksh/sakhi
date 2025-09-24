553bf5f2c74c4cdba584fc80e61cc99ac0dc55a3
Karan Sharma <karan03945@gmail.com>
2025-09-24 09:49:17 +0530

    local network added to cros

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:["http://localhost:5173","https://imksh-sakhi.netlify.app"],
+    origin:["http://localhost:5173","https://imksh-sakhi.netlify.app","http://10.165.197.71:5173/"],
     credentials:true,
 }))
 
