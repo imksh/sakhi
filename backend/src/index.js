cb0553ad571a715facc85c319a566d6f7b04f277
Karan Sharma <karan03945@gmail.com>
2025-12-19 19:15:19 +0530

    fixed email 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,6 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 const port = process.env.PORT;
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: [
@@ -25,12 +22,11 @@ app.use(
       "http://10.140.16.71:5173",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
