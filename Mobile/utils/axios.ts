c077e4acf43ba0effd7173d38ff53e69d5ea5821
Karan Sharma <karan03945@gmail.com>
2025-12-21 12:41:30 +0530

    Fix folder casing: Mobile → mobile

--- Mobile/utils/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://sakhi-wt7s.onrender.com/api",
-  // baseURL: "http://10.140.16.71:5001/api",
-  withCredentials: true,
-});
