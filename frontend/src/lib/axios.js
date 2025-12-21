45b77d8879a4d19bf403a3dff581b14285f2ea29
Karan Sharma <karan03945@gmail.com>
2025-12-22 01:18:10 +0530

    login fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,5 +4,8 @@ export const api = axios.create({
   baseURL: "https://sakhi-wt7s.onrender.com/api",
   // baseURL: "http://localhost:5001/api",
   // baseURL: "http://10.140.16.71:5001/api",
+  headers: {
+    "x-platform": "web",
+  },
   withCredentials: true,
 });
